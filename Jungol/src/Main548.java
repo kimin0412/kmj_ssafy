7f8b3e5f3dd014abab47a146e7a6c908c22eb490
kimin0412 <kimin0412@gmail.com>
2020-02-05 14:52:02 +0900

    Jungol Upload

---
+++ Jungol/src/Main548.java
@@ -0,0 +1,13 @@
+
+public class Main548 {
+
+	public static void main(String[] args) {
+		for (int i = 2; i < 5; i++) {
+			for (int j = 1; j < 6; j++) {
+				System.out.printf("%d * %d = %2d   ", i, j, i*j);
+			}
+			System.out.println();
+		}
+	}
+
+}
