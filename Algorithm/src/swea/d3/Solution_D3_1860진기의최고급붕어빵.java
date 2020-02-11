fc663426ad8b85af0a3f07302c8749a89f0328bd
MinJee <kimin0412@gmail.com>
2020-02-11 21:59:40 +0900

    sync

--- Algorithm/src/swea/d3/Solution_D3_1860진기의최고급붕어빵.java
+++ Algorithm/src/swea/d3/Solution_D3_1860진기의최고급붕어빵.java
@@ -12,6 +12,15 @@ public class Solution_D3_1860진기의최고급붕어빵 {
 		System.setIn(new FileInputStream("res/D3_1860_진기의최고급붕어빵.txt"));
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
+		int T = Integer.parseInt(in.readLine());
+		for (int tc = 0; tc < T; tc++) {
+			st = new StringTokenizer(in.readLine(), " ");
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			int = Integer.parseInt(st.nextToken());
+			
+			
+		}
 	}
 
 }
